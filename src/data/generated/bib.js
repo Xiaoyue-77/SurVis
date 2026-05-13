6cf5795b1d9c429cb6c25440ac38972f13ecbcc7
Xiaoyue-77 <huxiaoyue011206@gmail.com>
2026-05-13 16:28:48 +0800

    全量覆盖仓库

---
+++ src/data/generated/bib.js
@@ -0,0 +1,126 @@
+﻿const generatedBibEntries = {
+    "alsaade2023cyber": {
+        "author": "Alsaade, Fawaz Waselallah and Al-Adhaileh, Mosleh Hmoud",
+        "journal": "Sensors",
+        "number": "8",
+        "pages": "4086",
+        "publisher": "Multidisciplinary Digital Publishing Institute",
+        "title": "Cyber attack detection for self-driving vehicle networks using deep autoencoder algorithms",
+        "type": "article",
+        "volume": "23",
+        "year": "2023"
+    },
+    "althunayyan2024robust": {
+        "author": "Althunayyan, Muzun and Javed, Amir and Rana, Omer",
+        "journal": "Vehicular Communications",
+        "pages": "100837",
+        "publisher": "Elsevier",
+        "title": "A robust multi-stage intrusion detection system for in-vehicle network security using hierarchical federated learning",
+        "type": "article",
+        "volume": "49",
+        "year": "2024"
+    },
+    "hoang2022detecting": {
+        "author": "Hoang, Thien-Nu and Kim, Daehee",
+        "journal": "Vehicular Communications",
+        "pages": "100520",
+        "publisher": "Elsevier",
+        "title": "Detecting in-vehicle intrusion via semi-supervised learning-based convolutional adversarial autoencoders",
+        "type": "article",
+        "volume": "38",
+        "year": "2022"
+    },
+    "javed2021canintelliids": {
+        "author": "Javed, Abdul Rehman and ur Rehman, Saif and Khan, Mohib Ullah and Alazab, Mamoun and Reddy, Thippa",
+        "journal": "IEEE transactions on network science and engineering",
+        "number": "2",
+        "pages": "1456--1466",
+        "publisher": "IEEE",
+        "title": "CANintelliIDS: Detecting in-vehicle intrusion attacks on a controller area network using CNN and attention-based GRU",
+        "type": "article",
+        "volume": "8",
+        "year": "2021"
+    },
+    "kang2016intrusion": {
+        "author": "Kang, Min-Joo and Kang, Je-Won",
+        "journal": "PloS one",
+        "number": "6",
+        "pages": "e0155781",
+        "publisher": "Public Library of Science San Francisco, CA USA",
+        "title": "Intrusion detection system using deep neural network for in-vehicle network security",
+        "type": "article",
+        "volume": "11",
+        "year": "2016"
+    },
+    "le2024multi": {
+        "author": "Le, Tien-Dat and Truong, Hoang Bao Huy and Kim, Daehee and others",
+        "journal": "Knowledge-Based Systems",
+        "pages": "112091",
+        "publisher": "Elsevier",
+        "title": "Multi-classification in-vehicle intrusion detection system using packet-and sequence-level characteristics from time-embedded transformer with autoencoder",
+        "type": "article",
+        "volume": "299",
+        "year": "2024"
+    },
+    "rai2025securing": {
+        "author": "Rai, Ritu and Grover, Jyoti and Sharma, Prinkle and Pareek, Ayush",
+        "journal": "Scientific Reports",
+        "number": "1",
+        "pages": "13820",
+        "publisher": "Nature Publishing Group UK London",
+        "title": "Securing the CAN bus using deep learning for intrusion detection in vehicles",
+        "type": "article",
+        "volume": "15",
+        "year": "2025"
+    },
+    "seo2018gids": {
+        "author": "Seo, Eunbi and Song, Hyun Min and Kim, Huy Kang",
+        "booktitle": "2018 16th annual conference on privacy, security and trust (PST)",
+        "organization": "IEEE",
+        "pages": "1--6",
+        "title": "GIDS: GAN based intrusion detection system for in-vehicle network",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "shi2024ids": {
+        "author": "Shi, Jiahao and Xie, Zhijun and Dong, Li and Jiang, Xianliang and Jin, Xing",
+        "journal": "Vehicular Communications",
+        "pages": "100830",
+        "publisher": "Elsevier",
+        "title": "IDS-DEC: A novel intrusion detection for CAN bus traffic based on deep embedded clustering",
+        "type": "article",
+        "volume": "49",
+        "year": "2024"
+    },
+    "song2020vehicle": {
+        "author": "Song, Hyun Min and Woo, Jiyoung and Kim, Huy Kang",
+        "journal": "Vehicular Communications",
+        "pages": "100198",
+        "publisher": "Elsevier",
+        "title": "In-vehicle network intrusion detection using deep convolutional neural network",
+        "type": "article",
+        "volume": "21",
+        "year": "2020"
+    },
+    "yang2022federated": {
+        "author": "Yang, Jianfeng and Hu, Jianling and Yu, Tianqi",
+        "journal": "Electronics",
+        "number": "22",
+        "pages": "3658",
+        "publisher": "MDPI",
+        "title": "Federated AI-enabled in-vehicle network intrusion detection for internet of vehicles",
+        "type": "article",
+        "volume": "11",
+        "year": "2022"
+    },
+    "zhang2023federated": {
+        "author": "Zhang, Hengrun and Zeng, Kai and Lin, Shuai",
+        "journal": "IEEE transactions on information forensics and security",
+        "pages": "1566--1579",
+        "publisher": "IEEE",
+        "title": "Federated graph neural network for fast anomaly detection in controller area networks",
+        "type": "article",
+        "volume": "18",
+        "year": "2023"
+    }
+};
